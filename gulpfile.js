dac48bc117ab4a05721b53d86c43b8db59467cbc
Newtsrock <andrewtfluck@gmail.com>
2016-08-12 15:49:56 -0400

    transfer from old repo

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+var gulp = require("gulp"),
+    sass = require("gulp-sass");
+
+gulp.task('sass', function () {
+    return gulp.src('styles/scss/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./styles/css'));
+});
+
+gulp.task('default', ['sass']);
